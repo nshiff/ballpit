ffb7f78f67298aaf24d33101c554d293ca07ac54
nate <nshiff@users.noreply.github.com>
2016-11-24 23:03:17 -0500

    ball will fly around the screen and bounce off the edges

--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
+
+
+
+var EDGE_TOP = 0;
+var EDGE_LEFT = 0;
+var EDGE_RIGHT = 310;
+var EDGE_BOTTOM = 200;
+
 function setup() {
-
+	createCanvas(EDGE_RIGHT, EDGE_BOTTOM);
 }
 
-function draw() {
-  ellipse(50, 50, 80, 80);
+
+var x = 50;
+var y = 50;
+var velocityx = 5;
+var velocityy = 5;
+var radius = 20;
+
+function draw(){
+
+	
+	
+	if(x > EDGE_RIGHT){
+		velocityx *= -1;
+	}
+	if(x < EDGE_LEFT){
+		velocityx *= -1;
+	}
+	if(y > EDGE_BOTTOM){
+		velocityy *= -1;
+	}
+	if(y < EDGE_TOP){
+		velocityy *= -1;
+	}
+	
+	x += velocityx
+	y += velocityy
+	ellipse(x, y, 2*radius, 2*radius);
 }
+
